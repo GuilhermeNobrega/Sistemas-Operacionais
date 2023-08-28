35d65e4224943c27edee900c051d9e7ac67d15ff
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-28 13:02:26 -0300

    Update BestfitWorstfitFirstfit.py

--- BestfitWorstfitFirstfit.py
+++ BestfitWorstfitFirstfit.py
@@ -2,7 +2,7 @@
 com os algoritmos Best-fit, Worst-fit e First-fit."""
 
 def menorvalor(vlr):
-    return min(vlr, key=lambda d: d['Value'])
+    return min(vlr, key=lambda a: a['Value'])
 #Um dicionário em Python não pode ter chaves duplicadas.
 BestFit = [
     {"Processo": "a", "Value": 10},
@@ -13,5 +13,9 @@ BestFit = [
 menor_valor = menorvalor(BestFit)
 print(menor_valor)
 x = 15
-salvar = (menor_valor['Value'])
-print(x-salvar)
+salvar = menor_valor['Value']
+resultado = (x-salvar)
+#print(resultado)
+print(f"Processo x com valor {x} irá ser adicionado a menor posição de memória: => {menor_valor}")
+menor_valor.update({'Value': resultado})
+print(menor_valor)
