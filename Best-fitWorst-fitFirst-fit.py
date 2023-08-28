2ba7fc985d8757181cb0f71fcf8cf6f18d9d1f8f
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-28 09:19:25 -0300

    Create Best-fitWorst-fitFirst-fit.py

---
+++ Best-fitWorst-fitFirst-fit.py
@@ -0,0 +1,10 @@
+"""Desenvolva um programa que simule o uso da alocação particionada dinâmica, 
+com os algoritmos Best-fit, Worst-fit e First-fit."""
+
+Bestfit = {
+    "Nome_Processo": "Processus_Xx",
+    "ID": 1014,
+    "abacte": "oi"
+}
+nome = "jo"
+print(f"nome:{Bestfit}")
