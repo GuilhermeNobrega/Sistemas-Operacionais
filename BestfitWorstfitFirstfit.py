4715497525af8e30aa430933360d005d426b678d
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-28 12:05:40 -0300

    Update BestfitWorstfitFirstfit.py

--- BestfitWorstfitFirstfit.py
+++ BestfitWorstfitFirstfit.py
@@ -1,24 +1,17 @@
 """Desenvolva um programa que simule o uso da alocação particionada dinâmica, 
 com os algoritmos Best-fit, Worst-fit e First-fit."""
 
-Bestfit = {
-    "Nome_Processo": "Processus_Xx",
-    "ID": 1014,
-    "abacte": "oi"
-}
-nome = "jo"
-print(f"nome:{Bestfit}")
+def menorvalor(vlr):
+    return min(vlr, key=lambda d: d['Value'])
+#Um dicionário em Python não pode ter chaves duplicadas.
+BestFit = [
+    {"Processo": "a", "Value": 10},
+    {"Processo": "b", "Value": 20},
+    {"Processo": "c", "Value": 5}
+]
 
-test = {}
-if not test:
-    print("Dicionário vazio")
-else:
-  print(f"test é {test}")
-
-def add():
-  print("Vamos adicionar processos ao dicionario")
-  test["Processus"] = "xadasd"
-  return f" retorno{test}"
-
-print()
-add()
+menor_valor = menorvalor(BestFit)
+print(menor_valor)
+x = 15
+salvar = (menor_valor['Value'])
+print(x-salvar)
