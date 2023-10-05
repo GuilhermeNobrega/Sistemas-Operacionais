3cab32429c6d074900a87372436059fd77485ea1
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-05 09:48:39 -0300

    Update AlocaçãoContígua.py

--- AlocaçãoContígua.py
+++ AlocaçãoContígua.py
@@ -3,4 +3,13 @@
 First-fit → primeiro bloco disponível
 Best-fit → o bloco que deixa menos espaço livre
 Worst-fit → o maior bloco de espaço livre'''
-print(hello world!")
+
+from colorama import Fore, Style
+
+cores = [Fore.RED, Fore.GREEN, Fore.BLUE]
+
+itens = ["[x]", "[y]", "[z]"]
+
+for cor, item in zip(cores, itens):
+    texto_formatado = f"{cor}{item}{Style.RESET_ALL}"
+    print(texto_formatado)
