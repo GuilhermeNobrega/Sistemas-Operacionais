b75d46a92bcad1c5a3951d82b856cb3ad01b36d8
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-05 09:32:18 -0300

    Create AlocaçãoContígua.py

---
+++ AlocaçãoContígua.py
@@ -0,0 +1,6 @@
+'''Desenvolva em qualquer linguagem que interprete a alocação contígua:
+
+First-fit → primeiro bloco disponível
+Best-fit → o bloco que deixa menos espaço livre
+Worst-fit → o maior bloco de espaço livre'''
+print(hello world!")
